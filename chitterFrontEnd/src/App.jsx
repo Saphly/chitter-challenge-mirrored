ad87666ce590bc54b646d63c5264f9198783fe13
KW <kee.wei26@gmail.com>
2023-08-14 18:24:47 +0100

    added a not found page

--- chitterFrontEnd/src/App.jsx
+++ chitterFrontEnd/src/App.jsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import HomePage from "./Components/HomePage";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
     <>
-      <div className="bg-purple-800 font-">
+      <div>
         <Header />
         <div>
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/*" element={<NotFound />} />
           </Routes>
         </div>
       </div>
---
+++ chitterFrontEnd/src/Components/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="d-flex flex-column align-items-center justify-content-center">
+      <div className="fs-1">OOPS!</div>
+      <div className="fs-3">Page not found</div>
+    </div>
+  );
+};
+
+export default NotFound;
